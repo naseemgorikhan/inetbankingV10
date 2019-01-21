6b9dcc20256e55b28bacc45283e9127d2c9f2aa2
naseem.akhtar <naseem.akhtar@192.168.2.18>
2019-01-20 19:25:50 -0500

    Changes have done on TestFile.java

--- InetBankingV10/src/test/java/com/inetbanking/testcases/TestFile.java
+++ InetBankingV10/src/test/java/com/inetbanking/testcases/TestFile.java
@@ -1,1 +1,4 @@
 //This is testing file created to check pull activity
+//This is testing file created to check pull activity
+//This is testing file created to check pull activity
+//This is testing file created to check pull activity
